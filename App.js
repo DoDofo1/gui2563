6e1ce35c27da57f82e480cd4ddf180947be5ff76
DoDofo1 <connors.svf0683@gmail.com>
2020-08-07 13:06:43 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {view,Text} from 'react-native';
+
+export default function App(){
+  return(
+    <View style={{
+      flex:1 ;
+      justifyContent:"center",
+      alignItems:"center",
+    }}>
+      <Text>Hello</Text>
+    </View>
+  )
+}
